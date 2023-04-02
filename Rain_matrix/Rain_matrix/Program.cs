3fcc24589d0c46b9ecd7ff768198469a4fbe4b85
Alan-Camacho-Restrepo <alanstiv2000@gmail.com>
2023-04-02 13:59:11 -0500

    I'm learning about Console and Convert classes[C

--- Rain_matrix/Rain_matrix/Program.cs
+++ Rain_matrix/Rain_matrix/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double sideA, sideB, result;
+
+Console.WriteLine("Enter side A: ");
+if (!double.TryParse(Console.ReadLine(), out sideA))
+{
+    Console.WriteLine("Invalid input for side A. Please enter a valid number.");
+    return;
+}
+
+Console.WriteLine("Enter side B: ");
+if (!double.TryParse(Console.ReadLine(), out sideB))
+{
+    Console.WriteLine("Invalid input for side B. Please enter a valid number.");
+    return;
+}
+
+result = sideA * sideB;
+
+Console.WriteLine("The result is {0:0.00}", result);
